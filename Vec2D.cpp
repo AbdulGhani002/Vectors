51e16d5fec65e8cd49257d59cebbbfb8e424ff06
Muhammad Abdul Ghani Qureshi <itsaghani@gmail.com>
2024-05-29 23:04:24 +0500

    Addition and substraction methods are added

--- Vec2D.cpp
+++ Vec2D.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "Vec2D.h"
+#include<iostream>
+using namespace std;
+
+int main(){
+    Vec2D vectr(19.1,20.5);
+    cout<<vectr.magnitude();
+    return 0;
+}
--- Vec2D.h
+++ Vec2D.h
@@ -7,9 +7,45 @@
 
 
 #include<math.h>
+#include <exception>
+#include <iostream>
 
-Vec2D::Vec2D() : x(0.0), y(0, 0) {}
+using namespace std;
 
+class Vec2D {
+public:
+    Vec2D() : x(0.0), y(0.0) {};
+
+    Vec2D(double valueX, double valueY) : x(valueX), y(valueY) {};
+
+    double magnitude() {
+        return std::sqrt((x * x) + (y * y));
+    }
+
+    void add(double vecX, double vecY) {
+        try {
+            Vec2D vector(vecX, vecY);
+            x += vecX;
+            y += vecY;
+        } catch (std::exception &e) {
+            std::cerr << e.what() << std::endl;
+        }
+    }
+
+    void substract(double vecX, double vecY) {
+        try {
+            Vec2D vector(vecX, vecY);
+            x -= vecX;
+            y -= vecY;
+        } catch (std::exception &e) {
+            std::cerr << e.what() << std::endl;
+        }
+    }
+
+private:
+    double x;
+    double y;
+};
 
 
 #endif //VECTORS_VEC2D_H
